9204065e63eca2d067f4ce6762e1aa5824b2a73c
Stanko2 <stanko@bezak.net>
2023-09-23 14:22:35 +0200

    parsed data

--- freefood/client.go
+++ freefood/client.go
@@ -4,34 +4,66 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/gocolly/colly"
+	"golang.org/x/net/html"
 )
 
-func Download() {
-	c := colly.NewCollector(colly.AllowedDomains("www.freefood.sk"))
-	c.OnHTML("#free-food .day-offer", func(e *colly.HTMLElement) {
-		day := e.DOM.Parent().Children().Nodes[0].FirstChild.Data
-		fmt.Println(day)
-		dayOffers := e.DOM.Children().Nodes
-		ret := make([]Food, len(dayOffers))
-		for i, dayOffer := range dayOffers {
-			ret[i].Name = strings.Split(dayOffer.FirstChild.NextSibling.Data, "A:")[0]
-			priceStr := dayOffer.LastChild.FirstChild.Data
-			price, err := strconv.ParseFloat(priceStr[0:len(priceStr) - 3], 64)
-			if err != nil {
-				panic(err)
-			}
-			ret[i].Price = price
-			fmt.Print(dayOffer.FirstChild.FirstChild.Data)
-			fmt.Print(" - ")
-			fmt.Print(ret[i].Name)
-			fmt.Print(" - ")
-			fmt.Println(price)
+func Parse(dayOffers []*html.Node) []Food {
+	ret := make([]Food, len(dayOffers))
+	for i, dayOffer := range dayOffers {
+		ret[i].Name = strings.Split(dayOffer.FirstChild.NextSibling.Data, "A:")[0]
+		priceStr := dayOffer.LastChild.FirstChild.Data
+		price, err := strconv.ParseFloat(priceStr[0:len(priceStr) - 3], 64)
+		if err != nil {
+			panic(err)
 		}
-		// day := e.DOM.Parent().Children().Find(".day-title").Text()
-		// fmt.Println(day)
+		ret[i].Price = price
+		if (dayOffer.FirstChild.FirstChild.Data == "P. ") {
+			ret[i].Type = Soup
+		} else {
+			ret[i].Type = Main
+		}
+		
+		// fmt.Print(dayOffer.FirstChild.FirstChild.Data)
+		// fmt.Print(" - ")
+		// fmt.Print(ret[i].Name)
+		// fmt.Print(" - ")
+		// fmt.Println(price)
+	}
+
+	return ret
+}
+
+func Atoi(param string) int {
+    n, _ := strconv.Atoi(param)
+    return n
+}
+
+func ParseRestaurant(root *colly.HTMLElement) DailyMenu {
+	day := strings.Split(root.DOM.Parent().Children().Nodes[0].FirstChild.Data, ", ")[1]
+	dateStr := strings.Split(day, ".")
+	date := time.Date(
+		Atoi(dateStr[2]),
+		time.Month(Atoi(dateStr[1])),
+		Atoi(dateStr[0]),
+		 0, 0, 0, 0, time.Local)
+	menus := Parse(root.DOM.Children().Nodes)
+	return DailyMenu{menus: menus, date: date}
+}
+
+func Download() map[Restaurant][]DailyMenu {
+	ret := make(map[Restaurant][]DailyMenu)
+	c := colly.NewCollector(colly.AllowedDomains("www.freefood.sk"))
+	c.OnHTML("#fayn-food .day-offer", func(e *colly.HTMLElement) {
+		ret[faynfood] = append(ret[faynfood], ParseRestaurant(e))
 	})
+
+	c.OnHTML("#free-food .day-offer", func(e *colly.HTMLElement) {
+		ret[freefood] = append(ret[freefood], ParseRestaurant(e))
+	})
+
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
 	})
@@ -40,4 +72,11 @@ func Download() {
 	if err != nil {
 		panic(err)
 	}
+	
+	fmt.Println(ret)
+	return ret
+}
+
+func first(i int, err error) {
+	panic("unimplemented")
 }
--- freefood/main.go
+++ freefood/main.go
@@ -5,12 +5,31 @@ import (
 )
 
 type DailyMenu struct {
-	soup Food
-	main []Food
+	menus []Food
 	date time.Time
 }
 
+type Restaurant int16 
+
+const (
+	freefood Restaurant = 0
+	faynfood Restaurant = 1
+)
+
+type Menu struct {
+	menus map[time.Weekday]DailyMenu
+	restaurant Restaurant
+}
+
+type FoodType int16
+
+const (
+	Soup FoodType = 1
+	Main FoodType = 0
+)
+
 type Food struct {
+	Type FoodType
 	Name string
 	Price float64
 }
